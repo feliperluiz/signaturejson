ddb9017b9d2cb5e42e90e554e1c40fad4f754373
Felipe <lippee@gmail.com>
2019-07-09 20:59:48 -0300

    New Commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var xhr = new XMLHttpRequest();
-var url = "https://hsmlab64.dinamonetworks.com/api/gen_rand";
+var url = "https://hsmlab63.dinamonetworks.com/api/gen_rand";
+var dadoStr = '{"len": 16}'
 xhr.open("POST", url, true);
 xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Authorization", "HSM 48D50D8E79ABBEFC5F711B4B517046B29E4F7D52229E1587DB570752E13A081A");
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
         var json = JSON.parse(xhr.responseText);
