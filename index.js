470fb6fbc40f2570db0a05ebc6bf41f662924baa
Felipe <lippee@gmail.com>
2019-08-09 17:55:37 -0300

    Adição do reporter

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-var xhr = new XMLHttpRequest();
-var url = "https://hsmlab63.dinamonetworks.com/api/gen_rand";
-var dadoStr = '{"len": 16}'
-xhr.open("POST", url, true);
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("Authorization", "HSM 48D50D8E79ABBEFC5F711B4B517046B29E4F7D52229E1587DB570752E13A081A");
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var json = JSON.parse(xhr.responseText);
-        console.log(json.email + ", " + json.password);
+let hashDocumento;
+
+function onChange(event) { 
+    var logFile = $('#input_file').get(0).files[0];
+    var readerFile = new FileReader();
+    readerFile.readAsBinaryString(logFile);
+    readerFile.onload = function(event){
+        var arrayBuffer = event.target.result;
+        hashDocumento = CryptoJS.SHA256(arrayBuffer).toString(CryptoJS.enc.Hex);
+        console.log(hashDocumento);
+             
     }
-};
-var data = JSON.stringify({"len": 16});
-xhr.send(data);
+}
+//cd c:/Users/Felipe/Desktop/TCC/Signature/reporter
+function Sign () {  
+
+  event.preventDefault();
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:5000/sign',
+        data: hashDocumento,
+        success: function(responseData, textStatus, jqXHR) 
+        {
+            console.log(responseData);
+        },
+        error: function (responseData, textStatus, errorThrown) 
+        {
+            console.warn(responseData, textStatus, errorThrown);
+            alert('failed - ' + textStatus);
+        }
+    });
+}
